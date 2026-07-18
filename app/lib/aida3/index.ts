2d1539d03c42fe18827a5b1eefbcb00c7fa1ea9a
chido261 <pcmaster_apatzingan@hotmail.com>
2026-07-18 13:59:47 -0600

    AIDA3: agrega index.ts

---
+++ app/lib/aida3/index.ts
@@ -0,0 +1,8 @@
+export * from "./core/contracts";
+export * from "./core/expert";
+export * from "./core/expertRegistry";
+export * from "./core/planValidator";
+export * from "./core/resultAggregator";
+export * from "./core/turnOrchestrator";
+export * from "./core/workingMemory";
+
