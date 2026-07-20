8b1d952c3eacc69dcac9169201a6be3f3fe63479
chido261 <pcmaster_apatzingan@hotmail.com>
2026-07-19 18:22:42 -0600

    feat(aida3): export common specialist infrastructure

--- app/lib/aida3/index.ts
+++ app/lib/aida3/index.ts
@@ -5,6 +5,7 @@ export * from "./core/planValidator";
 export * from "./core/resultAggregator";
 export * from "./core/turnOrchestrator";
 export * from "./core/workingMemory";
+export * from "./specialists";
 export * from "./protocols/contracts";
 export * from "./protocols/markdownProtocolParser";
 export * from "./protocols/protocolRepository";
